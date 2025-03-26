2c3fc5c534498f949a9cf9ddbf9f37b43ad8b469
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-03-26 23:23:25 +0530

    profile page

---
+++ my-next-app/app/dashboard/profile/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const DashboardPage: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">I am a Profile</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
